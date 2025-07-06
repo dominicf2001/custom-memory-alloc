21a993a3a9e1cb5f47d714c065dba1189dae6db4
Dominic Ferrando <dominicf2001@gmail.com>
2025-07-06 17:57:13 -0400

    Playing around with block implementation

--- block/main.c
+++ block/main.c
@@ -1,9 +1,10 @@
+#include <stdio.h>
 #include <sys/mman.h>
 #include <string.h>
 #include <sys/types.h>
 
 #define BLOCK_SIZE 64
-#define POOL_SIZE (BLOCK_SIZE * 1000)
+#define POOL_SIZE (BLOCK_SIZE * 10)
 #define NUM_OF_BLOCKS (POOL_SIZE / BLOCK_SIZE)
 
 int main(){
@@ -14,9 +15,24 @@ int main(){
  
     // init
     for (size_t i = 0; i < NUM_OF_BLOCKS; ++i){
+        int reachedLastBlock = (i == (NUM_OF_BLOCKS - 1));
+
         void* current_block = &pool_base[i * BLOCK_SIZE];
-        void* next_block = &pool_base[(i + 1) * BLOCK_SIZE];
+        void* next_block = reachedLastBlock ?
+            NULL :
+            &pool_base[(i + 1) * BLOCK_SIZE];
+ 
         *(void**)current_block = next_block;
+    }
+
+
+    for (size_t i = 0; i < NUM_OF_BLOCKS; ++i){
+        void* current_block = &pool_base[i * BLOCK_SIZE];
+        printf("Block %zu:", i);
+        for (size_t j = 0; j < BLOCK_SIZE; ++j){
+            printf("%d ", ((u_int8_t*) current_block)[j]); 
+        }
+        printf("\n");
     }
 
     return 0;
